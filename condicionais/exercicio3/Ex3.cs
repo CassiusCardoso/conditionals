59e413bd6d3e38983ee5d60ab9a1fc6fca29ef12
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-29 08:33:37 -0300

    (fix)

--- condicionais/exercicio3/Ex3.cs
+++ condicionais/exercicio3/Ex3.cs
@@ -1,57 +1,57 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace condicionais.exercicio3
-//{
-//    public class Ex3
-//    {
-//        public static void Main(string[] args)
-//        {
-//            Dictionary<string, double> produtos = new Dictionary<string, double>();
+namespace condicionais.exercicio3
+{
+    public class Ex3
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, double> produtos = new Dictionary<string, double>();
 
-//            Boolean continuar = true;
-//            while (continuar)
-//            {
+            Boolean continuar = true;
+            while (continuar)
+            {
 
-//            Console.Write("Digite o nome do produto (ou sair para encerrar): ");
-//            string produto = Console.ReadLine();
-//            if (produto.ToLower() == "sair")
-//            {
-//                continuar = false;
-//                break;
-//                }
+            Console.Write("Digite o nome do produto (ou sair para encerrar): ");
+            string produto = Console.ReadLine();
+            if (produto.ToLower() == "sair")
+            {
+                continuar = false;
+                break;
+                }
 
 
-//                Console.Write("Digite o preço: ");
-//                if(double.TryParse(Console.ReadLine(), out double preco))
-//                {
-//                    if (preco < 0)
-//                    {
-//                        Console.WriteLine("O preço não pode ser negativo. Tente novamente.");
-//                        continue;
-//                    }
-//                    produtos[produto] = preco;
-//                }
-//                else
-//                {
-//                    Console.WriteLine("Preço inválido. Tente novamente.");
-//                }
-//            }
+                Console.Write("Digite o preço: ");
+                if(double.TryParse(Console.ReadLine(), out double preco))
+                {
+                    if (preco < 0)
+                    {
+                        Console.WriteLine("O preço não pode ser negativo. Tente novamente.");
+                        continue;
+                    }
+                    produtos[produto] = preco;
+                }
+                else
+                {
+                    Console.WriteLine("Preço inválido. Tente novamente.");
+                }
+            }
 
-//            Console.WriteLine("\nProdutos cadastrados:");
-//            double total = 0;
+            Console.WriteLine("\nProdutos cadastrados:");
+            double total = 0;
 
-//            foreach(var item in produtos)
-//            {
-//                Console.WriteLine($"Produto: {item.Key}, Preço: R${item.Value:F2}");
-//                total += item.Value;
-//            }
+            foreach(var item in produtos)
+            {
+                Console.WriteLine($"Produto: {item.Key}, Preço: R${item.Value:F2}");
+                total += item.Value;
+            }
 
-//            Console.WriteLine($"\nTotal dos produtos: R${total:F2}");
+            Console.WriteLine($"\nTotal dos produtos: R${total:F2}");
 
-//        }
-//    }
-//}
+        }
+    }
+}
--- condicionais/exercicio4/Ex4.cs
+++ condicionais/exercicio4/Ex4.cs
@@ -1,33 +1,36 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace condicionais.exercicio4
-//{
-//    public class Ex4
-//    {
-//        public static void Main(string[] args)
-//        {
-//            Console.WriteLine(" ----------- NOTAS ALUNOS ----------- ");
+namespace condicionais.exercicio4
+{
+    public class Ex4
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" ----------- NOTAS ALUNOS ----------- ");
 
-//            Dictionary<string, List<float>> notasAlunos  = new Dictionary<string, List<float>>();
-//            notasAlunos.Add("João", new List<float> { 7.5f, 8.0f, 6.5f });
-//            notasAlunos.Add("Maria", new List<float> { 9.0f, 8.5f, 7.0f });
-//            notasAlunos.Add("Pedro", new List<float> { 6.0f, 5.5f, 7.0f });
+            Dictionary<string, List<float>> notasAlunos  = new Dictionary<string, List<float>>();
+            notasAlunos.Add("João", new List<float> { 7.5f, 8.0f, 6.5f });
+            notasAlunos.Add("Maria", new List<float> { 9.0f, 8.5f, 7.0f });
+            notasAlunos.Add("Pedro", new List<float> { 6.0f, 5.5f, 7.0f });
 
-//            Console.WriteLine("Digite o nome do aluno para ver as notas: ");
-//            string nomeAluno = Console.ReadLine();
-//            if (notasAlunos.ContainsKey(nomeAluno))
-//            {
-//                float media = notasAlunos[nomeAluno].Average();
-//                Console.WriteLine($"A nota do aluno {nomeAluno} é {media}");
-//            }
+            Console.WriteLine("Digite o nome do aluno para ver as notas: ");
+            string nomeAluno = Console.ReadLine();
+            if (notasAlunos.ContainsKey(nomeAluno))
+            {
+                float media = notasAlunos[nomeAluno].Average();
+                Console.WriteLine($"A nota do aluno {nomeAluno} é {media}");
+            }
 
-            
+            // Aluno com maior média
+            foreach (var aluno in notasAlunos) { 
+                Console.WriteLine($"Aluno: {aluno.Key}, Notas: {string.Join(", ", aluno.Value)}");
+            }
 
-//        }
+        }
 
-//    }
-//}
+    }
+}
